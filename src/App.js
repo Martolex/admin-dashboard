c1d7d59d61ae7d5c276d26569399b13b305b646a
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-10-05 15:03:16 +0530

    set up redux

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import logo from "./logo.svg";
 import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
 import Wrapper from "./components/Layout/Wrapper";
-
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux";
+import { PersistGate } from "redux-persist/integration/react";
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Wrapper />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <div className="App">
+            <Wrapper />
+          </div>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 }
 
